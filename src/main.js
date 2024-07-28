1be2d9192aad86c6b33959ae5e507e0cbcfaef0e
DmytroVasylenko6 <tredstoun651@ukr.net>
2024-07-28 18:17:13 +0300

    Added slider to testimonials

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.swiper', {
+    modules: [Autoplay],
+    loop: true,
+    spaceBetween: 32,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1280: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
+
 // const modal = document.querySelector('.js-modal');
 const openBtn = document.querySelector('.js-open-btn');
 const closeBtn = document.querySelector('.js-close-btn');

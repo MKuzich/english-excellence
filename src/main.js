3f481fd5059c85b1e20f1681a930762f9eedcf05
Natalia Kucheriava <nkucheryava@gmail.com>
2024-07-29 12:27:39 +0200

    one more fix in main.js

--- src/main.js
+++ src/main.js
@@ -28,6 +28,31 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+const mobileMenuRefs = {
+  mobileMenu: document.querySelector('.js-mobile-menu'),
+  openMenuBtn: document.querySelector('.js-open-menu'),
+  closeMenuBtn: document.querySelector('.js-close-menu'),
+  backdrop: document.querySelector('.js-backdrop-menu'),
+};
+
+const toggleMenu = () => {
+  const isMenuOpen =
+    mobileMenuRefs.openMenuBtn.getAttribute('aria-expanded') === 'true' ||
+    false;
+
+  mobileMenuRefs.mobileMenu.classList.toggle('is-open');
+  mobileMenuRefs.mobileMenu.setAttribute('aria-hidden', isMenuOpen);
+  mobileMenuRefs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+
+  mobileMenuRefs.backdrop.classList.toggle('is-visible');
+
+  if (!isMenuOpen) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+};
+
 mobileMenuRefs.openMenuBtn.addEventListener('click', toggleMenu);
 mobileMenuRefs.closeMenuBtn.addEventListener('click', toggleMenu);
 mobileMenuRefs.backdrop.addEventListener('click', toggleMenu);

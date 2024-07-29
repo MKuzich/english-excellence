99cb67c284f4cbf341e290868aeec5fbd18739e2
Natalia Kucheriava <nkucheryava@gmail.com>
2024-07-29 09:59:04 +0200

    bug fix, styles in header

--- src/main.js
+++ src/main.js
@@ -1,18 +1,36 @@
-// const modal = document.querySelector('.js-modal');
-const openBtn = document.querySelector('.js-open-btn');
-const closeBtn = document.querySelector('.js-close-btn');
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
 
-// const toggleModal = () => {
-//   modal.classList.toggle('is-open');
-// };
-
-// openBtn.onclick = toggleModal;
-// closeBtn.onclick = toggleModal;
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.swiper', {
+    modules: [Autoplay],
+    loop: true,
+    spaceBetween: 32,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1280: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
 
 const mobileMenuRefs = {
   mobileMenu: document.querySelector('.js-mobile-menu'),
   openMenuBtn: document.querySelector('.js-open-menu'),
   closeMenuBtn: document.querySelector('.js-close-menu'),
+  backdrop: document.querySelector('.js-backdrop-menu'),
 };
 
 const toggleMenu = () => {
@@ -23,9 +41,39 @@ const toggleMenu = () => {
   mobileMenuRefs.mobileMenu.classList.toggle('is-open');
   mobileMenuRefs.mobileMenu.setAttribute('aria-hidden', isMenuOpen);
   mobileMenuRefs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+
+  mobileMenuRefs.backdrop.classList.toggle('is-visible');
+
+  if (!isMenuOpen) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
 };
 
-console.log(mobileMenuRefs);
 
 mobileMenuRefs.openMenuBtn.addEventListener('click', toggleMenu);
 mobileMenuRefs.closeMenuBtn.addEventListener('click', toggleMenu);
+mobileMenuRefs.backdrop.addEventListener('click', toggleMenu);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('.nav-menu-item a');
+
+  function setActiveLink() {
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+    });
+
+    const activeLink = Array.from(navLinks).find(
+      link => link.getAttribute('href') === window.location.hash
+    );
+
+    if (activeLink) {
+      activeLink.classList.add('active');
+    }
+  }
+
+  window.addEventListener('hashchange', setActiveLink);
+  setActiveLink();
+});

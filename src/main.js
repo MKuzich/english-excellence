ff259876fc78e332bc9acbdf076453ec72f3611a
Igor <aigor9534@gmail.com>
2024-07-31 23:21:25 +0300

    add animation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '/js/mobileMenu.js';
 import '/js/swiper.js';
 import '/js/formHandler.js';
 import './js/headerHeight.js';
+import './js/numberAnimation.js';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
